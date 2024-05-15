faecdea5318065678e9109e5a1f12db7ce8d881c
mjpark <80474535+parkminju20211126@users.noreply.github.com>
2024-05-15 16:20:53 +0900

    feat : getOneAgency 성공

--- controllers/searchController.js
+++ controllers/searchController.js
@@ -1,11 +1,4 @@
-const db = require("../config/db");
-const searchModel = require("../models/searchModel");
-const jwt = require("jsonwebtoken");
-const {httpRequest} = require('../utils/httpRequest');
-const fetch = require('node-fetch');
-//컨트롤러 함수에서는 데이터베이스에서 부동산중개업소 정보를 조회하는 비즈니스 로직을 수행
-
-async function fetchAllData(sgg_nm, bjdong_nm) {
+async function fetchAllData(sgg_nm, bjdong_nm, cmp_nm = null) {
   let start = 1;
   let count = 1000;
   let hasMoreData = true;
@@ -23,7 +16,7 @@ async function fetchAllData(sgg_nm, bjdong_nm) {
       if (js.landBizInfo && js.landBizInfo.row) {
         const rows = js.landBizInfo.row;
 
-        for (const row of rows.filter(row => row.SGG_NM === sgg_nm && row.BJDONG_NM === bjdong_nm)) {
+        for (const row of rows.filter(row => row.SGG_NM === sgg_nm && row.BJDONG_NM === bjdong_nm && (!cmp_nm || row.CMP_NM === cmp_nm))) {
           const getRatingOptions = {
             host: "stop_bang_review",
             port: process.env.PORT,
@@ -77,38 +70,17 @@ exports.getAgency = async(req, res) => {
   }
 };
 exports.getOneAgency = async(req, res) => {
-  const sgg_nm = req.query.sgg_nm;
-  const bjdong_nm = req.query.bjdong_nm;
-  const cmp_nm = '%'+req.query.cmp_nm+'%';
-
+  const { sgg_nm, bjdong_nm, cmp_nm } = req.body;
+  console.log(sgg_nm, bjdong_nm, cmp_nm);
   try {
-    // 서울시 공공데이터 api
-    const apiResponse = await fetch(
-      `http://openapi.seoul.go.kr:8088/${process.env.API_KEY}/json/landBizInfo/1/1000/`
-    );
-    const js = await apiResponse.json();
-
-    // const rows = await searchModel.getAgenciesModel(sgg_nm, bjdong_nm);
-    const rows = js.landBizInfo.row;
-    const filtered = [];
-
-    for(const row of rows) {
-      if(row.CMP_NM.includes(req.body.cmp_nm)) {
-        filtered.push(row);
-        row.avg_rating = 0; // 여기 고치자 원채야
-        row.countReview = 0; // 여기 고치자 원채야
-      }
+    const filteredData = await fetchAllData(sgg_nm, bjdong_nm, cmp_nm);
+    if (filteredData.length > 0) {
+      return res.json(filteredData); // Return only the first matched agency
+    } else {
+      res.status(404).json({ message: "No matching agency found." });
     }
-    
-    return res.json(filtered);
-    // const rows = await searchModel.getOneAgencyModel(sgg_nm,bjdong_nm,cmp_nm);
-    // res.json({ rows: rows });
   } catch (err) {
-    console.error(err.stack)
+    console.error(`Error while processing request: ${err.stack}`);
+    res.status(500).json({ error: 'Failed to fetch data' });
   }
-  if (!res.headersSent) {  // 헤더가 이미 전송되지 않았는지 확인
-    res.status(500).json({ error: "Internal Server Error", details: err.stack });
-  }
-}
-
-
+};
